04f915e97c7bf6ecd3166ef4820cfc11a73d2dc8
yongsang930 <yongsang930@naver.com>
2022-02-06 23:30:22 +0900

    2022-02-06

--- guestbook/src/main/java/org/zerock/guestbook/service/GuestbookService.java
+++ guestbook/src/main/java/org/zerock/guestbook/service/GuestbookService.java
@@ -1,11 +1,15 @@
 package org.zerock.guestbook.service;
 
 import org.zerock.guestbook.dto.GuestbookDTO;
+import org.zerock.guestbook.dto.PageRequestDTO;
+import org.zerock.guestbook.dto.PageResultDTO;
 import org.zerock.guestbook.entity.Guestbook;
 
 public interface GuestbookService {
 
     Long register(GuestbookDTO dto);
+
+    PageResultDTO<GuestbookDTO, Guestbook> getList(PageRequestDTO requestDTO);
 
     default Guestbook dtoToEntity(GuestbookDTO dto) {
         Guestbook entity = Guestbook.builder()
@@ -15,5 +19,15 @@ public interface GuestbookService {
                 .writer(dto.getWriter())
                 .build();
         return entity;
+    }
+
+    default GuestbookDTO entityToDto(Guestbook entity) {
+        GuestbookDTO dto = GuestbookDTO.builder()
+                .gno(entity.getGno())
+                .title(entity.getTitle())
+                .content(entity.getContent())
+                .writer(entity.getWriter())
+                .build();
+        return dto;
     }
 }
